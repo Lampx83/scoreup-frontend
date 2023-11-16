2b4b84066e7d1ec92c82860fda88c36d3574c9ec
Duy Việt <tranviet211ccc@gmail.com>
2023-11-16 22:21:36 +0700

    xoá file listening cũ, sửa một số ở listening mới

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 const config = {
     apiUrl: 'http://101.96.66.219:8020/v1/api',
+    // apiUrl: 'http://127.0.0.1:3006/v1/api',
     // Các thông tin cấu hình khác có thể được thêm vào đây
   };
   
