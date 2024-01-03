458ae28e3c2bd6839a7e90d87ed02c6286662954
Duy Việt <tranviet211ccc@gmail.com>
2024-01-03 21:59:53 +0700

    xoá những file thừa, thêm logic login cho homepage và topNav

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,5 +1,19 @@
 import createTopNav from "./topNav.js";
+import { getCookie } from "./helpers/cookieFunctions.js";
 // import createFooter from './footer.js'
 
 createTopNav("home");
 // createFooter()
+
+//! get info if user is logged in
+const token = getCookie("token");
+if (token) {
+  const buttons = document.querySelector(".user-buttons");
+  buttons.style.display = "none";
+}
+else 
+{
+  const userActions = document.querySelector(".user-actions");
+  userActions.style.display = "none";
+}
+//! end get info if user is logged in
--- src/js/topNav.js
+++ src/js/topNav.js
@@ -1,3 +1,7 @@
+import {getCookie, setCookie} from "./helpers/cookieFunctions.js";
+import config from './config.js';
+const apiUrl = config.apiUrl;
+
 const createTopNav = (idPage = "") => {
   fetch("topNav.html")
     .then((response) => response.text())
@@ -7,6 +11,66 @@ const createTopNav = (idPage = "") => {
       if (idPage) {
         document.querySelector(`#${idPage}`).classList.add("active");
       }
+
+      //! get info if user is logged in
+      const token = getCookie("token");
+      if (token) {
+        const buttons = document.querySelector(".user-buttons");
+        buttons.style.display = "none";
+      }
+      else 
+      {
+        const userActions = document.querySelector(".user-actions");
+        userActions.style.display = "none";
+      }
+      //! end get info if user is logged in
+
+      //!login modal
+      const loginForm = document.querySelector("#login-form");
+      if (loginForm) {
+        const btnSubmitLogin = document.querySelector("#btnSubmitLogin");
+        btnSubmitLogin.addEventListener("click", (e) => {
+          const username = document.querySelector("#username").value;
+          const password = document.querySelector("#password").value;
+
+          const usernameRegex = /^(?!.*[\s!@#$%^&*()_+={}\[\]:;<>,.?/~`])\S+$/;
+              if (!usernameRegex.test(username)) {
+            alert("Username is invalid. (not null, no special characters, no space)");
+            return;
+          }
+          if (!password) {
+            alert("Password is invalid. (not null)");
+            return;
+          }
+
+          const data = {
+            username: username,
+            password: password,
+          };
+          fetch(`${apiUrl}/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.statusCode === 200)
+              {
+                alert("Login success!");
+                const rememberMe = document.querySelector("#remembder-me").checked;
+                setCookie("token", data.metadata.token, rememberMe ? 30 : 1);
+                window.location.reload();
+              }
+              else
+              {
+                alert(data.message);
+              }
+            });
+        });
+      }
+      //!end login modal
     });
 };
 
