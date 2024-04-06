f1cf5136e94365e415b89883e15ffc5db478a2ce
Diệp Anh Nguyễn <diepanhnguyen2807@gmail.com>
2024-04-06 15:51:33 +0700

    upload avatar

--- src/js/avatar.js
+++ src/js/avatar.js
@@ -1,5 +1,11 @@
-var loadFile = function (event) {
-    var image = document.getElementById("output");
-    image.src = URL.createObjectURL(event.target.files[0]);
-  };
-  
+let upload = document.getElementById("upload");
+let chosenImage = document.getElementById("chosen-image");
+
+upload.onchange = () => {
+  let reader = new FileReader();
+  reader.readAsDataURL(upload.files[0]);
+  console.log(upload.files[0]);
+  reader.onload=()=>{
+    chosenImage.setAttribute("src",reader.result);
+  }
+}
