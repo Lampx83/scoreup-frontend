2fb24489dc12be109951e63df336b926a67ec25d
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-07-01 23:58:17 +0700

    React

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    assetsDir: '.',
-    emptyOutDir: true,
-  },
-  optimizeDeps: {
-    include: ['@emotion/react', '@emotion/styled', '@mui/material'],
-  },
-  exclude: ['**/old/**'], // Ensure proper exclusion of 'old' directory
-});
+  plugins: [react()],
+})
