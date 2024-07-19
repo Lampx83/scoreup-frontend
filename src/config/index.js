7bdc4c00ffc53bc1cddbd640870e25122c6e5056
duyviet2101 <whoisduyviet@gmail.com>
2024-07-20 03:21:49 +0700

    link api

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 
 const config = {
-  API_URL: import.meta.env.VITE_API_URL || 'http://101.96.66.219:8020/v1/api',
+  API_URL: import.meta.env.VITE_API_URL || 'https://scoreup.whoisduyviet.id.vn/v1/api',
 }
 
 export default config;
